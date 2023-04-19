d6e496a5bf3f87b13b11477030486e93096355af
oscarlemstra <61353780+oscarlemstra@users.noreply.github.com>
2023-04-19 14:44:45 +0200

    TFE-2

--- pages/login.php
+++ pages/login.php
@@ -10,24 +10,29 @@
     <title>Login</title>
 </head>
 <body>
-    <form action="../actions/login.php" method="post">
-        <label for="first_name">Voornaam</label><br>
-        <input type="text" id="first_name" name="first_name" placeholder="Voornaam" autofocus required><br><br>
+    <div class="main-div">
+        <img class="img-s-m" src="../resources/img/nut_screwdriver.png" alt="logo icon">
+        
+        <!-- TO-DO delete <br> -->
 
-        <label for="last_name">Tussenvoegsel en achternaam</label><br>
-        <input type="text" id="last_name" name="last_name" placeholder="Achternaam" required><br><br>
+        <form class="element element-s-m" action="../actions/login.php" method="post">
+            <!-- <label for="first_name">Voornaam</label><br> -->
+            <input type="text" id="first_name" name="first_name" placeholder="Voornaam" autofocus required>
+            
+            <!-- <label for="last_name">Tussenvoegsel en achternaam</label><br> -->
+            <input type="text" id="last_name" name="last_name" placeholder="Achternaam" required>
 
-        <label for="password">Wachtwoord</label><br>
-        <input type="password" id="password" name="password" placeholder="Wachtwoord" required><br>
-
-        <?php
-            if (isset($_SESSION['error'])) {
-                echo "<p class='error'>".$_SESSION['error']."</p>";
-            }
-        ?>
+            <!-- <label for="password">Wachtwoord</label><br> -->
+            <input type="password" id="password" name="password" placeholder="Wachtwoord" required>
+        
+            <?php
+                if (isset($_SESSION['error'])) {
+                    echo "<p class='error'>".$_SESSION['error']."</p>";
+                }
+            ?>
     
-        <br>
-        <input type="submit" value="Login">
-    </form>
+            <input class="login-bt" type="submit" value="Login">
+        </form>
+    </div>
 </body>
 </html>
