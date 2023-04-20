9a5b90c93e56edf52289192a97171e9b559cb47a
oscarlemstra <61353780+oscarlemstra@users.noreply.github.com>
2023-04-20 14:35:15 +0200

    TFE-3

--- index.php
+++ index.php
@@ -6,14 +6,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/styles/main.css">
     <title>Home</title>
 </head>
-
 <body>
-    <p>Welkom <?php echo $_SESSION['user']['first_name']." ".$_SESSION['user']['last_name']; ?></p>
-    <p>dit is een test:<?php echo " de test werkt."; ?> </p>
+    <div>
+        <nav></nav>
+        <div>
+            <div></div>
+        </div>
+    </div>
 
-    <!-- test -->
-    <div style="border: black 2px solid; height: 100px; width: 100px;"></div>
+    <!-- <pre><?php //print_r($_SESSION['user']); ?></pre> -->
 </body>
 </html>
