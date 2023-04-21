8dbe66fb37d9c8e85d7b49cbaaac8cb2b6c173cc
oscarlemstra <61353780+oscarlemstra@users.noreply.github.com>
2023-04-21 15:08:51 +0200

    TFE-3

--- index.php
+++ index.php
@@ -11,9 +11,65 @@
 </head>
 <body>
     <div>
-        <nav></nav>
-        <div>
-            <div></div>
+        <nav class="flex-box align-items-center justify-content-space-between">
+            <img class="img-s-s" src="./resources/img/nut_screwdriver.png" alt="logo icon">
+
+            <div>
+                <a class="nav-link" href="">Rapportages</a>
+                <a class="nav-link" href="">Producten</a>
+                <a class="nav-link" href="">Locaties</a>
+                <a class="nav-link" href="">Werknemers</a>
+            </div>
+        </nav>
+        
+        <div class="flex-box">
+            <form class="" action="" method="post">
+                <select id="location" name="location">
+                    <option value="test1">test 1</option>
+                    <option value="test2">test 2</option>
+                </select>
+
+                <select id="product" name="product">
+                    <option value="test1">test 1</option>
+                    <option value="test2">test 2</option>
+                </select>
+
+                <?php
+                    if (isset($_SESSION['error'])) {
+                        echo "<p class='error'>".$_SESSION['error']."</p>";
+                    }
+                ?>
+        
+                <input class="" type="submit" value="Zoeken">
+            </from>
+
+            <div>
+                <div>
+                    <h1>Voorraad overzicht</h1>
+                </div>
+                <div>
+                    <table>
+                        <tr>
+                            <th>Product</th>
+                            <th>Type</th>
+                            <th>Fabriek</th>
+                            <th>In voorraad</th>
+                        </tr>
+                        <tr>
+                            <td>Futterkiste</td>
+                            <td>Maria</td>
+                            <td>Germany</td>
+                            <td>Germany</td>
+                        </tr>
+                        <tr>
+                            <td>Centro</td>
+                            <td>Francisco</td>
+                            <td>Mexico</td>
+                            <td>Mexico</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 
