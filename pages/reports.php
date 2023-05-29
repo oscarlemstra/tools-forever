261b1addfec5419fbbe78893d5c17d57c92e860a
oscarlemstra <61353780+oscarlemstra@users.noreply.github.com>
2023-05-29 16:52:30 +0200

    TFE-6

--- pages/reports.php
+++ pages/reports.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
     $_SESSION['url'] = __DIR__;
-    $_SESSION['access'] = 'all';
-    require_once "./includes/user_validation.php";
-    require_once "./actions/index.php";
+    $_SESSION['access'] = 'office';
+    require_once "../includes/user_validation.php";
+    // require_once "../actions/reports.php";
 ?>
 
 <!DOCTYPE html>
@@ -12,22 +12,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./resources/styles/main.css">
-    <title>Home</title>
+    <link rel="stylesheet" href="../resources/styles/main.css">
+    <title>Rapportages</title>
 </head>
 <body>
     <div class="main-div">
         <nav class="flex-box align-items-center justify-content-space-between">
             <div class="flex-box align-items-center gap-10">
-                <img class="img-s-s" src="./resources/img/nut_screwdriver.png" alt="logo icon">
-                <a class="nav-link nav-link-selected" href="">Home</a>
+                <img class="img-s-s" src="../resources/img/nut_screwdriver.png" alt="logo icon">
+                <a class="nav-link" href="../index.php">Home</a>
             </div>
 
             <div>
                 <?php
                     if ((int) $_SESSION['user']['role_id'] >= 2) {
-                        echo '<a class="nav-link" href="./pages/reports.php">Rapportages</a>';
-                        echo '<a class="nav-link" href="./pages/products.php">Producten</a>';
+                        echo '<a class="nav-link nav-link-selected" href="">Rapportages</a>';
+                        echo '<a class="nav-link" href="./products.php">Producten</a>';
                     }
 
                     if ((int) $_SESSION['user']['role_id'] === 3) {
@@ -39,47 +39,38 @@
         </nav>
         <div class="flex-box align-items-flex-start gap-20">
             <form class="element element-s-s p-10 mt-45" action="" method="post">
-                <?php // a select element for location
-                    echo '<select class="mb-10" id="location" name="location" required>';
-                    echo '<option value="">Locatie</option>';
-
-                    foreach ($_SESSION['locations'] as $location) {
-                        if (!empty($_POST)) {
-                            if ($location['id'] === $_POST['location']) {
-                                echo '<option value="'.$location['id'].'" selected>'.$location['place_name'].'</option>';
-                            } else {
-                                echo '<option value="'.$location['id'].'">'.$location['place_name'].'</option>';
-                            }
-                        } else {
-                            if ($location['id'] === $_SESSION['user']['location_id']) {
-                                echo '<option value="'.$location['id'].'" selected>'.$location['place_name'].'</option>';
-                            } else {
-                                echo '<option value="'.$location['id'].'">'.$location['place_name'].'</option>';
-                            }
-                        }
-                    }
-                    echo '</select>';
-                ?>
-
-                <?php // a select element for prduct
-                    echo '<select class="mb-10" id="product" name="product">';
-                    echo '<option value="">Alle Product</option>';
-
-                    foreach ($_SESSION['product_names'] as $product_name) {
-                        if ($product_name['id'] === $_POST['product']) {
-                            echo '<option value="'.$product_name['id'].'" selected>'.$product_name['name'].'</option>';
-                        } else {
-                            echo '<option value="'.$product_name['id'].'">'.$product_name['name'].'</option>';
-                        }
-                    }
-                    echo '</select>';
-                ?>
-        
-                <input class="main-bt mt-25" type="submit" value="Zoeken">
+                <select class="mb-10" id="report" name="report" required>
+                    <option value="">Rapportage</option>
+                    <option value="total_stock">Totale voorraad</option>
+                    <option value="stock_value">Voorraad waarde</option>
+                    <option value="order_list">Bestellijst</option>
+                </select>
+                
+                <input class="main-bt mt-25" type="submit" value="Weergeven">
             </form>
             <div>
                 <div>
-                    <h1>Voorraad overzicht</h1>
+                    <?php // loads the right title for the report
+                        if (isset($_POST['report'])) {
+                            $report = $_POST['report'];
+                        } else {
+                            $report = "";
+                        }
+
+                        switch ($report) {
+                            case "total_stock":
+                                echo "<h1>Rapportage - Totale voorraad</h1>";
+                                break;
+                            case "stock_value":
+                                echo "<h1>Rapportage - Voorraad waarde</h1>";
+                                break;
+                            case "order_list":
+                                echo "<h1>Rapportage - Bestellijst</h1>";
+                                break;
+                            default:
+                                echo "<h1>Rapportages</h1>";
+                        }
+                    ?>
                 </div>
                 <div class="element element-s-l p-15">
                     <?php
@@ -87,11 +78,11 @@
                             echo "<p class='error'>".$_SESSION['error']."</p>";
                         }
 
-                        if (empty($_SESSION['products']) && empty($_SESSION['error'])) {
+                        if (empty($_SESSION['report']) && empty($_SESSION['error'])) {
                             echo "<p>Er zijn geen resultaten gevonden.</p>";
                         }
 
-                        if (!empty($_SESSION['products']) && empty($_SESSION['error'])) {
+                        if (!empty($_SESSION['report']) && empty($_SESSION['error'])) {
                     ?>
                     <table>
                         <thead>
@@ -103,10 +94,10 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <?php // create's <tr> elements with the products data
-                                foreach ($_SESSION['products'] as $product) {
+                            <?php // create's <tr> elements with the report data
+                                foreach ($_SESSION['report'] as $report) {
                                     echo '<tr>';
-                                    foreach ($product as $index => $value) {
+                                    foreach ($report as $index => $value) {
                                         if (gettype($index) === "string") {
                                             echo '<td>'.$value.'</td>';
                                         } else {
